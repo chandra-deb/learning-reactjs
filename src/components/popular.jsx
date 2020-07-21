f3d1651447bad13926d4df66554604ef81d3bd39
chandra-deb <mail.chandradeb@gmail.com>
2020-07-21 22:38:15 +0600

    completed repos grid design and logic

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { fetchPopularRepos } from "../utils/api";
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle,
+} from "react-icons/fa";
 
 function LanguagesNav({ selected, onUpdate }) {
   const languages = ["All", "Javascript", "Python", "Ruby", "Rust", "Julia"];
@@ -27,16 +33,72 @@ LanguagesNav.propTypes = {
   onUpdate: PropTypes.func.isRequired,
 };
 
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues,
+        } = repo;
+        const { login, avatar_url } = owner;
+        return (
+          <li key={html_url} className="repo bg-light">
+            <h4 className="header-lg center-text">#{index + 1}</h4>
+            <img
+              src={avatar_url}
+              alt={`avatar for ${login}`}
+              className="avatar"
+            />
+            <h2 center-text>
+              <a href={html_url} className="link">
+                {login}
+              </a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color="rgb(255, 191, 116)" size={22} />
+                <a href={`https://github.com/${login}`}>{login}</a>
+              </li>
+              <li>
+                <FaStar color="rgb(255, 215, 0)" size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                {open_issues.toLocaleString()} open
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+
 class Popular extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       selectedLanguage: "All",
-      repos: null,
+      repos: {},
       error: null,
     };
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
 
   componentDidMount() {
@@ -44,23 +106,34 @@ class Popular extends Component {
   }
 
   updateLanguage(selectedLanguage) {
-    this.setState({ selectedLanguage, repos: null, error: null });
+    this.setState({ selectedLanguage, error: null });
 
-    fetchPopularRepos(selectedLanguage)
-      .then((repos) => this.setState({ repos, error: null }))
-      .catch((error) => {
-        console.warn("Error while fetching repos", error);
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data,
+            },
+          }));
+        })
+        .catch((error) => {
+          console.warn("Error while fetching repos", error);
 
-        this.setState({ error: "There was an error fetching the reops!" });
-      });
+          this.setState({ error: "There was an error fetching the reops!" });
+        });
+    }
   }
 
   isLoading() {
-    return this.state.repos === null && this.state.error === null;
+    const { selectedLanguage, error, repos } = this.state;
+    return !repos[selectedLanguage] && error === null;
   }
 
   render() {
     const { selectedLanguage, repos, error } = this.state;
+    console.log(repos);
     return (
       <React.Fragment>
         <LanguagesNav
@@ -70,7 +143,9 @@ class Popular extends Component {
 
         {this.isLoading() && <h2>Loading</h2>}
         {error && <p>Error</p>}
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]} />
+        )}
       </React.Fragment>
     );
   }
